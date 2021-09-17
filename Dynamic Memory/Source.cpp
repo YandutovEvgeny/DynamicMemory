21cdfba590aa6cb667002a8ee5a163754a97e5ec
yandutka@mail.ru <yandutka@mail.ru>
2021-09-17 21:26:44 +0300

    commit

--- Dynamic Memory/Source.cpp
+++ Dynamic Memory/Source.cpp
@@ -95,7 +95,7 @@ int* push_back(int arr[], int& n, int r_value)
 }
 int* push_front(int arr[], int& n, int l_value)
 {
-	int* buffer = new int[n + 2];
+	int* buffer = new int[n + 1];
 	for (int i = 0; i < n; i++)
 	{
 		buffer[i + 1] = arr[i];
@@ -109,11 +109,11 @@ int* push_front(int arr[], int& n, int l_value)
 }
 int* insert(int arr[], int& n, int insert_value, int insert_index)
 {
-	int* buffer = new int[n + 3];
+	int* buffer = new int[n + 1];
 	for (int i = 0; i < insert_index; i++)
 	{
 		buffer[i] = arr[i];
-		buffer[insert_index] = insert_value;
+		//buffer[insert_index] = insert_value;
 	}
 	for (int i = insert_index + 1; i < n + 1; i++)
 	{
@@ -143,22 +143,20 @@ int* pop_front(int arr[], int& n)
 		buffer[i] = arr[i + 1];
 	}
 	delete[] arr;
-	/*arr = buffer;
-	//arr[n] = NULL;
-	n--;*/
 	return buffer;
 }
 int* erase(int arr[], int& n, int pop_index)
 {
-	int* buffer = new int[n - 1];
-	for (int i = pop_index; i < n - 1; i++)
+	int* buffer = new int[--n];
+	for (int i = 0; i < pop_index; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	for (int i = pop_index; i < n; i++)
 	{
 		buffer[i] = arr[i + 1];
 	}
 	delete[] arr;
 	arr = buffer;
-	//arr[pop_index] = NULL;
-	n--;
 	return arr;
-	
 }

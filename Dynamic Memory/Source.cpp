bece4be1f9f222e668b2526058b839f7e78e5d24
yandutka@mail.ru <yandutka@mail.ru>
2021-10-05 21:46:34 +0300

    commit

--- Dynamic Memory/Source.cpp
+++ Dynamic Memory/Source.cpp
@@ -1,68 +1,48 @@
-﻿#include<iostream>
-using std::cout;
+﻿#include<iostream>  //Директива языка Си
+using std::cout;    //using - Директива языка С++
 using std::cin;
 using std::endl;
 //#define DYNAMIC_MEMORY_1
 #define DYNAMIC_MEMORY_2
+#define DATA_TYPE double
+
 
 //Заполнение массивов & вывод на экран:
-template <typename T>
-void FillRand(T arr[], const unsigned int n, int minRand = 0, int maxRand = 100);
-template <typename T>
-void FillRand(T** arr, const unsigned int rows, const unsigned int cols, int minRand = 0, int maxRand = 100);
-void FillRand(double** arr, const unsigned int rows, const unsigned int cols);
-void FillRand(float** arr, const unsigned int rows, const unsigned int cols);
-template <typename T>
-void Print(T arr[], const unsigned int n);
-template <typename T>
-void Print(T** arr, const unsigned int rows, const unsigned int cols);
+template <typename T>void FillRand(T arr[], const unsigned int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const unsigned int n, int minRand = 0, int maxRand = 100);
+void FillRand(char arr[], const unsigned int n);
+template <typename T>void FillRand(T** arr, const unsigned int rows, const unsigned int cols, int minRand = 0, int maxRand = 100);
+void FillRand(double** arr, const unsigned int rows, const unsigned int cols, int minRand = 0, int maxRand = 100);
+void FillRand(char** arr, const unsigned int rows, const unsigned int cols);
+template <typename T>void Print(T arr[], const unsigned int n);
+template <typename T>void Print(T** arr, const unsigned int rows, const unsigned int cols);
 
 //Создание и удаление двумерного массива:
-template <typename T>
-T** allocate(unsigned int rows, unsigned int cols);
-template <typename T>
-void clear(T** arr, const unsigned int rows);
+template <typename T>T** allocate(unsigned int rows, unsigned int cols);
+template <typename T>void clear(T** arr, const unsigned int rows);
 
 //Основные функции для одномерного массива:
-template <typename T>
-T* push_back(T arr[], int& n, int r_value);
-template <typename T>
-T* push_front(T arr[], int& n, int l_value);
-template <typename T>
-T* insert(T arr[], int& n, int insert_value, int insert_index);
-template <typename T>
-T* pop_back(T arr[], int& n);
-template <typename T>
-T* pop_front(T arr[], int& n);
-template <typename T>
-T* erase(T arr[], int& n, int pop_index);
+template <typename T>T* push_back(T arr[], int& n, int r_value);
+template <typename T>T* push_front(T arr[], int& n, int l_value);
+template <typename T>T* insert(T arr[], int& n, int insert_value, int insert_index);
+template <typename T>T* pop_back(T arr[], int& n);
+template <typename T>T* pop_front(T arr[], int& n);
+template <typename T>T* erase(T arr[], int& n, int pop_index);
 
 //Основные функции для двумерного массива:
-template <typename T>
-T** push_row_back(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-T** push_row_front(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-T** insert_row(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int insert_index);
-template <typename T>
-T** pop_row_back(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-T** pop_row_front(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-T** erase_row(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int pop_index);
+template <typename T>T** push_row_back(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>T** push_row_front(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>T** insert_row(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int insert_index);
+template <typename T>T** pop_row_back(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>T** pop_row_front(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>T** erase_row(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int pop_index);
 
-template <typename T>
-void push_col_back(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-void push_col_front(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-void insert_col(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int insert_index);
-template <typename T>
-void pop_col_back(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-void pop_col_front(T**& arr, unsigned int& rows, unsigned int& cols);
-template <typename T>
-void erase_col(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int pop_index);
+template <typename T>void push_col_back(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>void push_col_front(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>void insert_col(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int insert_index);
+template <typename T>void pop_col_back(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>void pop_col_front(T**& arr, unsigned int& rows, unsigned int& cols);
+template <typename T>void erase_col(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int pop_index);
 
 void main()
 {
@@ -112,7 +92,8 @@ void main()
 	unsigned int cols;
 	cout << "Введите количество строк: "; cin >> rows;
 	cout << "Введите количество элементов строки: "; cin >> cols;
-	float** arr = allocate<float>(rows, cols);
+	
+	DATA_TYPE** arr = allocate<DATA_TYPE>(rows, cols);
 	/////////////////////////////////////////////////////////////////////////////
 	////////  Обращение к элементам двумерного динамического массива:    ////////
 	/////////////////////////////////////////////////////////////////////////////
@@ -198,6 +179,23 @@ void FillRand(T arr[], const unsigned int n, int minRand, int maxRand)
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void FillRand(double arr[], const unsigned int n, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = double(rand() % (maxRand - minRand) + minRand);
+		arr[i] /= 100;
+	}
+}
+void FillRand(char arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = char(rand());
 	}
 }
 template <typename T>
@@ -319,23 +317,26 @@ void FillRand(T** arr, const unsigned int rows, const unsigned int cols, int min
 		}
 	}
 }
-void FillRand(double** arr, const unsigned int rows, const unsigned int cols)
+void FillRand(double** arr, const unsigned int rows, const unsigned int cols, int minRand, int maxRand)
 {
+	minRand *= 100;
+	maxRand *= 100;
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			arr[i][j] = double(rand() % 10000) / 100;
+			arr[i][j] = double(rand() % (maxRand - minRand) + minRand);
+			arr[i][j] /= 100;
 		}
 	}
 }
-void FillRand(float** arr, const unsigned int rows, const unsigned int cols)
+void FillRand(char** arr, const unsigned int rows, const unsigned int cols)
 {
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			arr[i][j] = float(rand() % 10000) / 100;
+			arr[i][j] = rand();
 		}
 	}
 }
@@ -513,7 +514,7 @@ void push_col_front(T**& arr, unsigned int& rows, unsigned int& cols)
 template <typename T>
 void insert_col(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int insert_index)
 {
-	if (insert_index >= rows)cout << "Индекс больше массива!\n";
+	if (insert_index >= cols)cout << "Индекс больше массива!\n";
 	else
 	{
 		for (int i = 0; i < rows; i++)
@@ -566,10 +567,10 @@ void pop_col_front(T**& arr, unsigned int& rows, unsigned int& cols)
 template <typename T>
 void erase_col(T**& arr, unsigned int& rows, unsigned int& cols, unsigned int pop_index)
 {
-	if (pop_index >= rows)cout << "Индекс больше массива!\n";
+	if (pop_index >= cols)cout << "Индекс больше массива!\n";
 	else
 	{
-		for (int i = 0; i < rows; i++)
+	    for (int i = 0; i < rows; i++)
 		{
 			T* buffer = new T[cols - 1];
 			for (int j = 0; j < pop_index; j++)
